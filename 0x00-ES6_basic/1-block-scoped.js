7a95e847ce84b787673d41374b8492ce5aa718d2
AshrafAtef <alxashraf0@gmail.com>
2024-07-22 17:32:43 +0300

    first task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,5 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
-
   return [task, task2];
 }
